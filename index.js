c62d74aa3a1d1b720ac601b87eddb9592a7e2d35
IvanCandido <ivanncandido@gmail.com>
2022-01-20 17:14:51 -0300

    Funcao de rota

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-const filmesServices = require("./services/FilmesServices")
+const express = require("express");
 
-console.log(filmesServices.listar())
+const servidor = express();
+
+servidor.get("/", (req, res) =>{
+    console.log("Alguem fez uma requisiçao");
+    res.send("toma ai o q voce me pediu");
+})
+
+servidor.get("/filmes", (req, res) =>{
+    console.log("Filme assistido");
+    res.send("Homem Aranha");
+})
+
+servidor.get("/generos", (req, res) =>{
+    console.log("Generos do Filme");
+    res.send("comedia");
+})
+
+servidor.get("/filmes/:posicao", (req,res)=>{
+
+    let posicao = req.params.posicao;
+    const filmes = require("./database/filmes.json")
+    res.send(filmes[posicao])
+})
+
+
+servidor.listen(3000)
